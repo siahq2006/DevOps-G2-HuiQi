0bc6b8a0cbb1cd66aec39248b0fbcfe83ea2f475
Jas1vp <2303692D@student.tp.edu.sg>
2024-11-04 21:31:20 +0800

    implemented and tested out both delete and update

--- utils/Movie_jasper.js
+++ utils/Movie_jasper.js
@@ -1,1 +1,102 @@
-[]
+[
+  {
+    "movie_name": "Black Widow",
+    "poster_url": "https://m.media-amazon.com/images/I/71xpPnyQOIL._AC_UF894,1000_QL80_.jpg",
+    "description": "In Marvel Studios' action-packed spy thriller \"Black Widow,\" Natasha Romanoff aka Black Widow confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.",
+    "genre": "action",
+    "rating": "4.1",
+    "release_date": "2021-07-09",
+    "duration": "133",
+    "id": "1730272231217194"
+  },
+  {
+    "movie_name": "Captain America: Civil War",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_FMjpg_UX1000_.jpg",
+    "description": "Steve Rogers leads the newly formed team of Avengers in their continued efforts to safeguard humanity. But after another incident involving the Avengers results in collateral damage, political pressure mounts to install a system of accountability, headed by a governing body to oversee and direct the team.",
+    "genre": "action",
+    "rating": "4.6",
+    "release_date": "2016-04-21",
+    "duration": "148",
+    "id": "1730272364448559"
+  },
+  {
+    "movie_name": "Marley and Me",
+    "poster_url": "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p176014_p_v10_ae.jpg",
+    "description": "Newlyweds John and Jenny Grogan (Owen Wilson, Jennifer Aniston) leave behind snowy Michigan and move to Florida, where they buy their first home and find jobs at competing newspapers. Soon afterward, the Grogans adopt Marley, an adorable yellow Labrador pup. But Marley soon grows up to be a mischievous handful.",
+    "genre": "family",
+    "rating": "4.6",
+    "release_date": "2008-12-25",
+    "duration": "115",
+    "id": "1730272454352104"
+  },
+  {
+    "movie_name": "The Conjuring",
+    "poster_url": "https://i.ebayimg.com/00/s/MTYwMFgxMDgw/z/7~oAAOSw-89ZTBuI/$_57.JPG?set_id=8800005007",
+    "description": "https://upload.wikimedia.org/wikipedia/en/8/8c/The_Conjuring_poster.jpg",
+    "genre": "horror",
+    "rating": "4.6",
+    "release_date": "2013-08-08",
+    "duration": "112",
+    "id": "1730272563334669"
+  },
+  {
+    "movie_name": "Star Wars: Episode IX - The Rise of Skywalker",
+    "poster_url": "https://m.media-amazon.com/images/I/81oApl4ir9L._AC_UF1000,1000_QL80_.jpg",
+    "description": "Set after The Last Jedi, The Rise of Skywalker follows Rey, Finn, and Poe Dameron as they lead the remnants of the Resistance in a final stand against Supreme Leader Kylo Ren and the First Order, who are allied by the resurrected Sith Lord, Emperor Palpatine.",
+    "genre": "sci-fi",
+    "rating": "3.3",
+    "release_date": "2019-12-19",
+    "duration": "142",
+    "id": "1730451402291717"
+  },
+  {
+    "movie_name": "Just for Meeting You",
+    "poster_url": "https://i.mydramalist.com/Z85pYO_4f.jpg",
+    "description": "Xu Nian Nian and Yang Yi met at the most beautiful age and experienced the most memorable moments of youth. It's also a period of their lives that will eventually be forgotten. When the two people who have never forgotten each other meet again, can they be brave enough to overcome all difficulties?",
+    "genre": "romance",
+    "rating": "5",
+    "release_date": "2023-08-22",
+    "duration": "108",
+    "id": "1730531838145363"
+  },
+  {
+    "movie_name": "Pandora (판도라)",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZjRkMjAxMjgtZTUwMC00MDI0LWI3YTYtYzVjZDFkMWQ1YTkyXkEyXkFqcGc@._V1_.jpg",
+    "description": "An earthquake strikes the small town where Jae-Hyeok lives and causes explosions at the nuclear power plant. The situation quickly spirals out of control, leading the entire nation to panic. To prevent another nuclear disaster, Jae-Hyeok and his co-workers return to the nuclear power plant.",
+    "genre": "2",
+    "rating": "4.8",
+    "release_date": "2016-12-07",
+    "duration": "136",
+    "id": "1730534704658061"
+  },
+  {
+    "movie_name": "Jumanji: Welcome To The Jungle",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZjI5MzdmODctYjA4NS00ZmMxLWJlOWUtOGVhMjA0OGMxMWYzXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    "description": "Jumanji is about four American teenagers, who magically get sucked into a game and become the characters that they chose. By going on a quest, and overcoming challenges, the fantastic four have to work as a team to return to the real world.",
+    "genre": "2",
+    "rating": "4.6",
+    "release_date": "2017-12-21",
+    "duration": "119",
+    "id": "1730537025882335"
+  },
+  {
+    "movie_name": "Avengers: Infinity War",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_FMjpg_UX1000_.jpg",
+    "description": "Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality.",
+    "genre": "2",
+    "rating": "4.7",
+    "release_date": "2018-04-26",
+    "duration": "149",
+    "id": "1730538566031477"
+  },
+  {
+    "movie_name": "A Dog's Journey",
+    "poster_url": "https://upload.wikimedia.org/wikipedia/en/d/d5/A_Dogs_Journey_Movie_Poster_Art.jpg",
+    "description": "Molly is reincarnated as a male English mastiff named Big Dog, who lives with a convenience store owner in Pennsylvania. One day, CJ visits the shop and is lovingly greeted by Big Dog. He mistakenly believes that CJ recognizes him, is heartbroken when she leaves without him, and spends the rest of his life missing her.",
+    "genre": "3",
+    "rating": "4.8",
+    "release_date": "2019-05-16",
+    "duration": "109",
+    "id": "1730555126190784"
+  }
+]
