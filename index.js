75892522a56474775c263e354bc560a3210dcc25
Kai Low <2302761H@student.tp.edu.sg>
2024-11-07 23:45:00 +0800

    Implemented genre filter

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+
 const PORT = process.env.PORT || 5050
 var startPage = "index.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const {viewMovies, getGenres, viewMovieByGenre} = require('./utils/MovieUtil');
+app.get('/viewMovies', viewMovies);
+app.get('/getGenres', getGenres);
+app.get('/viewMovieByGenre/:id', viewMovieByGenre);
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
 server = app.listen(PORT, function () {
     const address = server.address();
-    const baseUrl = `http://${address.address == "::" ? 'localhost' :
-        address.address}:${address.port}`;
+    const baseUrl = `http://${address.address == "::" ? 'localhost' : address.address}:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`);
 });
+
 module.exports = { app, server }
