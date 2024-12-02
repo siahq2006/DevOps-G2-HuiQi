4d3a43d5c5a073f246c6331543bae54f2d4316a2
Hui Qi <siahuiqi2006@gmail.com>
2024-12-02 09:04:38 +0800

    Created and tested the test cases for addMovie API testing

--- test/MovieUtil.test.js
+++ test/MovieUtil.test.js
@@ -1,15 +1,189 @@
-{
-    "reporter": [
-        "text",
-        "html"
-    ],
-        "include": [
-            "utils/*.js"
-        ],
-            "reportDir": "coverage/backend",
-                "check-coverage": true,
-                    "branches": 80,
-                        "lines": 80,
-                            "functions": 80,
-                                "statements": 80
-}
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const { app, server } = require('../index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+
+let baseUrl;
+describe('Movies API', () => {
+    let consoleErrorSpy;
+
+    before(async () => {
+        const { address, port } = await server.address();
+        baseUrl = `http://${address == '::' ? 'localhost' : address}:${port}`;
+    });
+    after(() => {
+        return new Promise((resolve) => {
+            server.close(() => {
+                resolve();
+            });
+        });
+    });
+
+    let count = 0;
+    let resourceId; // Variable to store the ID of the movies
+
+    // Test Suite for viewing movies
+    describe('GET /viewMovies_HuiQi', () => {
+        it('should return all movies', (done) => {
+            chai.request(baseUrl)
+                .get('/viewMovies_HuiQi')
+                .end((err, res) => {
+                    count = res.body.length;
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    done();
+                });
+        });
+    });
+
+    describe('GET /getGenres_HuiQi', () => {
+        it('should return all movies', (done) => {
+            chai.request(baseUrl)
+                .get('/getGenres_HuiQi')
+                .end((err, res) => {
+                    count = res.body.length;
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    genres = res.body;
+                    done();
+                });
+        });
+    });
+
+    // Test Suite for adding movies
+    describe('POST /addMovie', () => {
+        it('should return 400 if all fields are not filled in', (done) => {
+            chai.request(baseUrl)
+                .post('/addMovie')
+                .send({
+                    movie_name: 'Test Movie',
+                    poster_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vintagemovieposters.co.uk%2Fshop%2Favengers-movie-poster%2F&psig=AOvVaw0mtX5Pd-yRZXteC9Gzvr53&ust=1732676893784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj978iC-YkDFQAAAAAdAAAAABAE',
+                    description: 'Hello World!!!',
+                    genre: "",
+                    rating: "4.5",
+                    release_date: "2021-09-01",
+                    duration: "123"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body.message).to.equal('All fields are required!');
+                    done();
+                });
+        });
+
+        it('should return 400 if description is less than 10 characters long', (done) => {
+            chai.request(baseUrl)
+                .post('/addMovie')
+                .send({
+                    movie_name: 'Test Movie',
+                    poster_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vintagemovieposters.co.uk%2Fshop%2Favengers-movie-poster%2F&psig=AOvVaw0mtX5Pd-yRZXteC9Gzvr53&ust=1732676893784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj978iC-YkDFQAAAAAdAAAAABAE',
+                    description: 'short',
+                    genre: genres[1].id,
+                    rating: "4.5",
+                    release_date: "2021-09-01",
+                    duration: "123"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body.message).to.equal('Description must be at least 10 characters long!');
+                    done();
+                });
+        });
+
+        it('should return 400 if rating is less than 1 or more than 5', (done) => {
+            chai.request(baseUrl)
+                .post('/addMovie')
+                .send({
+                    movie_name: 'Test Movie',
+                    poster_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vintagemovieposters.co.uk%2Fshop%2Favengers-movie-poster%2F&psig=AOvVaw0mtX5Pd-yRZXteC9Gzvr53&ust=1732676893784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj978iC-YkDFQAAAAAdAAAAABAE',
+                    description: 'Hello World!!!',
+                    genre: genres[1].id,
+                    rating: "0",
+                    release_date: "2021-09-01",
+                    duration: "123"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body.message).to.equal('Rating must be a number between 1 and 5!');
+                    done();
+                });
+        });
+
+        it('should return 400 if release date is not in YYYY-MM-DD format', (done) => {
+            chai.request(baseUrl)
+                .post('/addMovie')
+                .send({
+                    movie_name: 'Test Movie',
+                    poster_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vintagemovieposters.co.uk%2Fshop%2Favengers-movie-poster%2F&psig=AOvVaw0mtX5Pd-yRZXteC9Gzvr53&ust=1732676893784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj978iC-YkDFQAAAAAdAAAAABAE',
+                    description: 'Hello World!!!',
+                    genre: genres[1].id,
+                    rating: "4.5",
+                    release_date: "21-09-2001",
+                    duration: "123"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body.message).to.equal('Release date must be in the format YYYY-MM-DD!');
+                    done();
+                });
+        });
+
+        it('should return 400 if duration is not positive', (done) => {
+            chai.request(baseUrl)
+                .post('/addMovie')
+                .send({
+                    movie_name: 'Test Movie',
+                    poster_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vintagemovieposters.co.uk%2Fshop%2Favengers-movie-poster%2F&psig=AOvVaw0mtX5Pd-yRZXteC9Gzvr53&ust=1732676893784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj978iC-YkDFQAAAAAdAAAAABAE',
+                    description: 'Hello World!!!',
+                    genre: genres[1].id,
+                    rating: "4.5",
+                    release_date: "2021-09-01",
+                    duration: "0"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body.message).to.equal('Duration must be a positive integer!');
+                    done();
+                });
+        });
+
+        it('should add a new movie', (done) => {
+            console.log("Genres: ", genres); // Confirm genre availability
+            chai.request(baseUrl)
+                .get('/viewMovies_HuiQi')  // Ensure we get the count of movies before adding a new one
+                .end((err, res) => {
+                    if (err) done(err);
+
+                    const countBeforeAdd = res.body.length; // Get the number of movies before adding
+
+                    chai.request(baseUrl)
+                        .post('/addMovie')
+                        .send({
+                            movie_name: 'Test Movie',
+                            poster_url: 'https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg',
+                            description: 'Hello World!!!',
+                            genre: genres[1].id,
+                            rating: "4.5",
+                            release_date: "2021-09-01",
+                            duration: "123"
+                        })
+                        .end((err, res) => {
+                            expect(res).to.have.status(201);
+                            expect(res.body).to.be.an('array'); // Ensure response is an array
+
+                            const newCount = res.body.length; // Get the new count of movies
+                            expect(newCount).to.equal(countBeforeAdd + 1); // Compare with the count before the add operation
+
+                            resourceId = res.body[newCount - 1].id; // Store the ID of the newly added movie
+                            done();
+                        });
+                });
+        });
+
+    });
+
+});
+
--- utils/MovieUtils_HuiQi.js
+++ utils/MovieUtils_HuiQi.js
@@ -10,6 +10,7 @@ async function readJSON(filename) {
     } catch (err) { console.error(err); throw err; }    // If there's an error, log it and throw it for handling
 }
 
+
 // Writes an object to a JSON file by appending it to existing data
 async function writeJSON(object, filename) {
     try {
@@ -31,29 +32,32 @@ async function writeJSON(object, filename) {
 async function addMovie(req, res) {
     try {
         // Extract movie data from the request body
-        const movie_name = req.body.movie_name;
-        const poster_url = req.body.poster_url;
-        const description = req.body.description;
-        const genre = req.body.genre;
-        const rating = req.body.rating;
-        const release_date = req.body.release_date;
-        const duration = req.body.duration;
+        const { movie_name, poster_url, description, genre, rating, release_date, duration } = req.body;
 
-        // Basic validation
-        if (description.length < 10 || !genre || !movie_name || !poster_url || !rating || !release_date || !duration) {
-            return res.status(400).json({ message: 'Validation error' });
-        } else {
-            // Create a new Movie instance
-            const newMovie = new Movie(movie_name, poster_url, description, genre, rating, release_date, duration);
-
-            // Assuming writeJSON function writes to a JSON file and returns updated list
-            const updatedMovies = await writeJSON(newMovie, 'utils/movies.json');
-            return res.status(201).json(updatedMovies);
+        // Basic validation: check if all fields are provided
+        if (!description || !genre || !movie_name || !poster_url || !rating || !release_date || !duration) {
+            return res.status(400).json({ message: 'All fields are required!' });
+        } else if (description.length < 10) {
+            return res.status(400).json({ message: 'Description must be at least 10 characters long!' });
+        } else if (isNaN(rating) || rating < 1 || rating > 5) {
+            return res.status(400).json({ message: 'Rating must be a number between 1 and 5!' });
+        } else if (!/^\d{4}-\d{2}-\d{2}$/.test(release_date)) {
+            return res.status(400).json({ message: 'Release date must be in the format YYYY-MM-DD!' });
+        } else if (!Number.isInteger(Number(duration)) || duration <= 0) {
+            return res.status(400).json({ message: 'Duration must be a positive integer!' });
         }
+
+        // Create a new Movie instance
+        const newMovie = new Movie(movie_name, poster_url, description, genre, rating, release_date, duration);
+
+        // Assuming writeJSON function writes to a JSON file and returns updated list
+        const updatedMovies = await writeJSON(newMovie, 'utils/movies.json');
+        return res.status(201).json(updatedMovies);
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
 }
+
 
 
 // Endpoint to fetch all movies from the JSON file
@@ -65,7 +69,6 @@ async function viewMovies_HuiQi(req, res) {
         return res.status(500).json({ message: error.message });
     }
 }
-
 
 // Endpoint to fetch all Genres from the JSON file
 async function getGenres_HuiQi(req, res) {
